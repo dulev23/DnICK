1dabbfc92b097b381769822994706c5ad0d825c1
Nikola Dulev <nikoladulev13@gmail.com>
2025-05-12 23:43:38 +0200

    Added forms and views

--- FlightApp/FlightApp/views.py
+++ FlightApp/FlightApp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+
+from .forms import ContactForm, FlightForm
 from .models import Flight
 from datetime import datetime
+
 
 # Create your views here.
 def index(request):
@@ -9,7 +12,44 @@ def index(request):
     context = {'flight_list': flights, 'app_name': 'FlightApp'}
     return render(request, 'index.html', context)
 
+
 def details(request, flight_id):
     flight = Flight.objects.filter(id=flight_id).first()
     context = {'flight_data': flight, 'app_name': 'FlightApp'}
     return render(request, 'details.html', context)
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass
+        return redirect('index')
+
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact.html', context)
+
+
+def add_flight(request):
+    if request.method == 'POST':
+        form = FlightForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+
+    form = FlightForm()
+    context = {'form': form}
+    return render(request, 'add_flight.html', context)
+
+def edit_flight(request, flight_id):
+    flight = get_object_or_404(Flight, pk=flight_id)
+    if request.method == 'POST':
+        form = FlightForm(request.POST, request.FILES, instance=flight)
+        if form.is_valid():
+            form.save()
+
+        return redirect('index')
+    form = FlightForm(instance=flight)
+    context = {'form': form}
+    return render(request, 'edit_flight.html', context)
